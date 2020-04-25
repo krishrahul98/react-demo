2d8654c382433ec7d5285b2c9db86ddcceeda46a
krishrahul98 <krishrahul98@gmail.com>
2020-04-26 01:51:04 +0530

    Used Styled Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import "./App.css";
 import Person from "./Person/Person";
+
+const StyleButton = styled.button`
+  background-color: ${(props) => (props.buttonColor ? "red" : "green")};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) =>
+      props.buttonColor ? "salmon" : "lightgreen"};
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -41,19 +56,6 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: "green",
-      color: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-      ":hover": {
-        backgroundColor: "lightgreen",
-        color: "black",
-      },
-    };
-
     let persons = null;
 
     if (this.state.showPersons) {
@@ -72,11 +74,6 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = "red";
-      style[":hover"] = {
-        backgroundColor: "salmon",
-        color: "black",
-      };
     }
 
     const classes = [];
@@ -91,9 +88,12 @@ class App extends Component {
       <div className='App'>
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <StyleButton
+          buttonColor={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
           Toggle Persons
-        </button>
+        </StyleButton>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,26 @@
 import React from "react";
-import "./Person.css";
+import styled from "styled-components";
+
+const StyleDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 3px solid #eee;
+  padding: 16px;
+  text-align: center;
+
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
 
 const person = (props) => {
-  //console.log(children);
-  const style = {
-    "@media (min-width: 500px)": {
-      width: "450px",
-    },
-  };
   return (
-    <div className='Person' style={style}>
+    <StyleDiv>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old.
       </p>
       <input type='text' onChange={props.changed} defaultValue={props.name} />
-    </div>
+    </StyleDiv>
   );
 };
 export default person;
