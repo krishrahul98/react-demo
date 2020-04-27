bd1cb9dbd2ebab04331ef5c12108e499caeef588
krishrahul98 <krishrahul98@gmail.com>
2020-04-28 02:15:39 +0530

    Updates

---
+++ precache-manifest.ed5bb497d6cdb25104ade8e749c95bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288c92e869642ea412616e42aad22494",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "7461a9bb0c48fbe2432a",
+    "url": "/react-demo/static/css/main.964f3059.chunk.css"
+  },
+  {
+    "revision": "1d70a47dacfcfe760835",
+    "url": "/react-demo/static/js/2.5d806f6a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-demo/static/js/2.5d806f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7461a9bb0c48fbe2432a",
+    "url": "/react-demo/static/js/main.55164907.chunk.js"
+  },
+  {
+    "revision": "3fc7955796e75a0df26e",
+    "url": "/react-demo/static/js/runtime-main.968c302e.js"
+  }
+]);
